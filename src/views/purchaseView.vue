1c0caaa0e5ee0eec72d5f2525f5898b184d83aa8
Hussein-A4 <smartron4cr7@gmail.com>
2022-12-11 17:46:03 -0500

    added site modal and movies to purchase page

--- src/views/purchaseView.vue
+++ src/views/purchaseView.vue
@@ -1,10 +1,42 @@
 <script setup>
+import { useStore } from '../store/index.js'
+import { storeToRefs } from 'pinia'
+import modal from "../components/siteModal.vue"
+
+const main = useStore()
+
+var moviesData = []
+var cartData = []
+
+// extract specific store properties
+const { movies, getMovies } = storeToRefs(main)
+
+main.getMovies()
+moviesData = movies
+console.log(moviesData)
+
+const openModal = () => {
+    console.log('clicked')
+    //check if exists to avoid duplicates
+}
+
+const checkout = () => {
+    console.log('checkout')
+}
 </script>
 
 <template>
     <h1>Purchase</h1>
+    <img v-for="movie in moviesData" :src="movie.poster" @click="openModal(movie.id)"/>
+    <button @click="checkout()"> </button>
+    <div class="siteModal">
+        <modal />
+    </div>
 </template>
 
 <style scoped>
-
+img{
+    aspect-ratio: 2/3;
+    width: 200px;
+}
 </style>
